37b305597d6026b9d8e5ce91fbd99dcd7eabd68d
Marselena <github.seaplanes@simplelogin.co>
2022-12-26 16:12:28 -0500

    Add text content for visualization

--- script.js
+++ script.js
@@ -4,5 +4,6 @@ const container = document.querySelector(".container");
 // append 16 divs to container div
 for (i=0; i<16; i++) {
     const test = document.createElement("div")
+    test.textContent = i
     container.appendChild(test)
 }
