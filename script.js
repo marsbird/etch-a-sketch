6c0a745059522549028642cde12680f42703eec4
Marselena <github.seaplanes@simplelogin.co>
2022-12-26 16:45:34 -0500

    Listen for hover on divs

--- script.js
+++ script.js
@@ -2,8 +2,17 @@
 const container = document.querySelector(".container");
 
 // append 16 divs to container div
-for (i=0; i<16; i++) {
-    const test = document.createElement("div")
-    test.textContent = i
-    container.appendChild(test)
+for (i = 0; i < 16; i++) {
+    const test = document.createElement("div");
+    test.textContent = i;
+    container.appendChild(test);
 }
+
+// listen for hover on divs
+const divs = document.querySelectorAll(".container > div");
+divs.forEach((div) => {
+    div.addEventListener("mouseover",() => {
+        console.log(div)
+    });
+});
+
