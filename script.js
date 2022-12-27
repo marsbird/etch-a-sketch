3dddfff2c782998757e66329ad7f68f7151b921d
Marselena <github.seaplanes@simplelogin.co>
2022-12-27 13:14:58 -0500

    Prompt user for number of rows

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-// select container div
+// select elements
+const btn = document.querySelector("button")
 const container = document.querySelector(".container");
+
+// set default number of rows
+let numRows = 10;
+
+// listen for button click
+btn.addEventListener("click", () => {
+    let userInput = prompt("How many rows? (max 100)");
+    let errorMessage = "Please input a number between 1 and 100"
+
+    if (isNaN(userInput)) {
+        alert(errorMessage);
+    } else {
+        let number = parseInt(userInput);
+        
+        if (number < 1 || number > 100) {
+            alert(errorMessage);
+        } else {
+            numRows = number;
+        };
+    };
+});
 
 // append divs to container div
 for (i = 0; i < 100; i++) {
